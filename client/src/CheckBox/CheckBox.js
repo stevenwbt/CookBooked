6c9c358a1aad267f9d90b8d233212139ce9c01b9
Juntab1 <jsbwhaleguy@gmail.com>
2024-05-09 18:10:03 -0700

    comments and clean up

--- client/src/CheckBox/CheckBox.js
+++ client/src/CheckBox/CheckBox.js
@@ -1,8 +1,13 @@
 import React, {useState} from 'react';
+// Import specific components and utilities from React Native for building the user interface.
 import {CheckBox, Text, View} from 'react-native';
+// Import specific styles
 import styles from './CheckBox.style'
 
-const CheckBoxed = (checkedVisible, setCheckedVisible) => {
+// This is for checking and unchecking items within the receipt.
+// checkedVisible (boolean for if we can see it) and setCheckedVisible (update checkedVisible)
+// are hooks(properties) that control the visibility of the CheckedBox.
+const CheckBoxed = ({checkedVisible, setCheckedVisible}) => {
 
   return (
     <View style={styles.container}>
